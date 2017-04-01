c759409663d440976c045a737b01e4fe28b648dc
Melissa Jane Farrow <melissajanefarrow@Melissas-MacBook-Air.local>
2017-04-01 15:40:26 +1100

    some very minor tidy up

--- src/scripts/dialogue_tree.js
+++ src/scripts/dialogue_tree.js
@@ -3,9 +3,9 @@ import {story} from './story.js'
 const question = document.getElementById("question")
 const options = document.getElementById("options")
 
-const question_template = (reply) => {
-  return `<p onclick="render_text(${reply.next})">${reply.response}</p>`
-}
+const question_template = (reply) => (
+  `<p onclick="render_text(${reply.next})">${reply.response}</p>`
+)
 
 const next_button_template = (current) => {
   let html
@@ -20,15 +20,8 @@ const next_button_template = (current) => {
 export const render_text = (current) => {
   let text = story[current].text
   let replies = story[current].replies
-
   question.innerHTML = text
-
   if(replies.length){
-    let html = replies.map(reply => {
-      return `<p>
-        ${reply.response}
-      </p>`
-    })
     options.innerHTML = replies.map(reply => question_template(reply)).join('')
   }else{
     options.innerHTML = next_button_template(current)
@@ -38,5 +31,6 @@ export const render_text = (current) => {
 export const dialogue_tree = () => {
   //make render text available to call in onclick
   window.render_text = render_text
+  //render tree from the start
   render_text(0)
 }
