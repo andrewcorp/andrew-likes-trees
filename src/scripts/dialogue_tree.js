65ce0bf700eb7e79ff52d1986a0b85997041f494
Andrew Ellis <andrew@upstartpark.com>
2017-04-04 08:42:34 +1000

    change fallback font to courier. add wheel styles. add basic dialogue structure. modify render link function to actually render hyperlinks as well

--- src/scripts/dialogue_tree.js
+++ src/scripts/dialogue_tree.js
@@ -3,22 +3,33 @@ import {story} from './story.js'
 const question = document.getElementById("question")
 const options = document.getElementById("options")
 
-const render_link = (index,text) => `<p onclick="render_text(${index})">${text}</p>`
+const render_link = (index,text,link='',render_button) => {
+  if(link.length){
+    return `<a href="${link}">${text}</a>`
+  }else{
+    return `<p class="${render_button ? "next" : "response"}" onclick="render_text(${index})">
+              ${text}
+            </p>`
+  }
+}
 
-const question_button = (reply) => render_link(reply.next, reply.response)
+const question_button = (reply) => render_link(reply.next, reply.response, reply.link, false)
 
 const next_button = (current) => {
   let text = story[current+1] ? "next" : "start again"
   let index = story[current+1] ? current + 1 : 0
-  return render_link(index,text)
+  return render_link(index,text,'',true)
 }
 
 const render_text = (current) => {
   let text = story[current].text
   let replies = story[current].replies
   question.innerHTML = text
-  if(replies.length){
-    options.innerHTML = replies.map(reply => question_button(reply)).join('')
+  if(replies && replies.length){
+    options.innerHTML = `
+      ${replies.map(reply => question_button(reply)).join('')}
+      <div class="wheel"</div>
+    `
   }else{
     options.innerHTML = next_button(current)
   }
--- src/scripts/story.js
+++ src/scripts/story.js
@@ -1,41 +1,128 @@
 export const story = [
   {
-    text: "Hello, my name is Andrew. You've stumbled upon my portfolio",
+    text: "Hello there",
+    // text: "Hello, my name is Andrew. You've stumbled upon my portfolio",
     replies: []
   },
   {
-    text: "Sometimes I like to stand in the street at night and drink beer.",
+    text: "Intro 1",
     replies: []
   },
   {
-    text: "How are you,anyway?",
+    text: "Intro 2",
+    replies: []
+  },
+  {
+    text: "Intro 3",
+    replies: []
+  },
+  {
+    text: "How are you, anyway?",
     replies: [
-      { response: "Bad", next: 3 },
-      { response: "Good", next: 4 }
+      //dont care about the answer really
+      { response: "Good", next: 5 },
+      { response: "Bad", next: 5 }
     ]
   },
   {
-    text: "That's a shame. Is there anything I can do to help?",
+    text: "Ok. Is there anything you'd like to ask me?",
     replies: [
-      { response: "Yes", next: 4 },
-      { response: "No", next: 6 }
+      { response: "What do you do?", next: 7 },
+      { response: "Why don't you tell me about yourself?", next: 6 },
+      { response: "How can I get in touch with you?", next: 16 },
+      { response: "Tell me something cool", next: 17 }, //random fact generator,
+      { response: "Nah that's OK", next: 19 }  //go to end
     ]
   },
   {
-    text: "That's great. Lets move on though. Do you want to hear what I have to say?",
+    //work
+    text: "We talking in a work sense or on a deeper level?",
     replies: [
-      { response: "Yes", next: 0 },
-      { response: "No", next: 6 }
+      { response: "Work", next: 8 }, // go to work 1
+      { response: "Lets get deep", next: 12 }, // go to self 1
     ]
   },
   {
-    text: "Are you sure?",
+    //work
+    text: "Well I make websites. I like to dabble in stuff like",
+    replies: []
+  },
+  {
+    //work
+    text: "Work 1",
+    replies: []
+  },
+  {
+    //work
+    text: "Work 2",
+    replies: []
+  },
+  {
+    //work
+    text: "Work 3",
+    replies: []
+  },
+  {
+    //repeat work
+    text: "And that's pretty much it. Have you got all that or do you need me to start again?",
     replies: [
-      { response: "Yes", next: 6 },
-      { response: "No", next: 2 }
+      { response: "Got it.", next: 5 },
+      { response: "I wasn't paying attention.", next: 7 },
+      { response: "I've gotta go.", next: 19 } //go to end
     ]
   },
   {
+    //about self
+    text: "About self 1",
+    replies: []
+  },
+  {
+    //about self
+    text: "About self 2",
+    replies: []
+  },
+  {
+    //about self
+    text: "About self 3",
+    replies: []
+  },
+  {
+    //repeat self
+    text: "Now you know everything there is to know. Did you need me to go over anything again?",
+    replies: [
+      { response: "Tell me about 1", next: 12 },
+      { response: "Tell me about 2", next: 13 },
+      { response: "Tell me about 3", next: 14 },
+      { response: "OK thanks. I feel like I really know you.", next: 5 },
+      { response: "I've gotta go.", next: 19 } //go to end
+    ]
+  },
+  {
+    //contact
+    text: "I've got a few ways you can see what I'm up to.",
+    replies: [
+      { response: "Phone", link: 'http://www.google.com'},
+      { response: "Email", link: 'http://www.google.com'},
+      { response: "Github", link: 'http://www.google.com'},
+      { response: "LinkedIn", link: 'http://www.google.com'},
+      { response: "Ok cool", next: 5},
+    ]
+  },
+  {
+    //we're done here
+    text: "Random fact",
+    replies: []
+  },
+  {
+    //we're done here
+    text: "Wanna hear another one?",
+    replies: [
+      { response: "Alright", next: 17},
+      { response: "Nah", next: 5}
+    ]
+  },
+  {
+    //we're done here
     text: "Alright bye",
     replies: []
   }
